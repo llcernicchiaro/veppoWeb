a509f0e9c2166cdaf51191dbcbacda4b46407cf5
Lorenzo Cernicchiaro <lopescernicchiaro@gmail.com>
2020-02-06 15:49:44 -0300

    Initial commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,17 @@
+export default {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: 'daybreak',
+  layout: 'topmenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: false,
+  },
+  title: 'Veppo',
+  pwa: false,
+  iconfontUrl: '',
+};
